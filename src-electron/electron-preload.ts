105103b679510b7195fcb2c343e9bf21ff49e1a7
13adams37 <KAktycc1337@gmail.com>
2023-12-02 19:59:54 +0300

    fixed file patcher

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -1,53 +1,16 @@
-/**
- * This file is used specifically for security reasons.
- * Here you can access Nodejs stuff and inject functionality into
- * the renderer thread (accessible there through the "window" object)
- *
- * WARNING!
- * If you import anything from node_modules, then make sure that the package is specified
- * in package.json > dependencies and NOT in devDependencies
- *
- * Example (injects window.myAPI.doAThing() into renderer thread):
- *
- *   import { contextBridge } from 'electron'
- *
- *   contextBridge.exposeInMainWorld('myAPI', {
- *     doAThing: () => {}
- *   })
- *
- * WARNING!
- * If accessing Node functionality (like importing @electron/remote) then in your
- * electron-main.js you will need to set the following when you instantiate BrowserWindow:
- *
- * mainWindow = new BrowserWindow({
- *   // ...
- *   webPreferences: {
- *     // ...
- *     sandbox: false // <-- to be able to import @electron/remote in preload script
- *   }
- * }
- */
-
 import { FileFilter, contextBridge, ipcRenderer } from 'electron';
 import { BrowserWindow, dialog } from '@electron/remote';
 import * as fs from 'fs';
 import * as path from 'path';
 import { patchDocument } from 'docx';
 import { PatchType, TextRun, PatchDocumentOptions, IPatch } from 'docx';
-// import { IForm, IVariables} from '/src/ts/formJsonValidation';
 import { IForm, IVariables } from '../src/ts/formJsonValidation';
-
-// interface IVariables {
-//   name: string;
-//   value: string;
-// }
 
 function getFilePath(fileName: string, filters: FileFilter[]) {
   return dialog.showSaveDialog(null!, {
     title: 'Сохранить файл',
     defaultPath: fileName,
     buttonLabel: 'Сохранить',
-
     filters: filters,
   });
 }
@@ -91,26 +54,6 @@ contextBridge.exposeInMainWorld('myWindowAPI', {
     const formData: IForm = JSON.parse(formData_in) as IForm;
 
     let folderPath: string = '';
-    let notPatchError: Error | boolean = true;
-
-    // async function patchSelectedDocument(
-    //   documents: IPath[],
-    //   patch_list: PatchDocumentOptions
-    // ) {
-    //   documents.forEach((document) => {
-    //     patchDocument(fs.readFileSync(document.path), patch_list)
-    //       .then((doc) => {
-    //         fs.writeFileSync(
-    //           `${folderPath}\\${path.parse(document.path).base}`,
-    //           doc
-    //         );
-    //       })
-    //       .catch((error: Error) => {
-    //         console.log('error in patch', error);
-    //         notPatchError = error;
-    //       });
-    //   });
-    // }
 
     function getElementsToPatch(element: IVariables[]): PatchDocumentOptions {
       const patches: { [key: string]: IPatch } = {};
@@ -120,7 +63,6 @@ contextBridge.exposeInMainWorld('myWindowAPI', {
           children: [new TextRun(patch.value)],
         };
       });
-      console.log('got elements to patch.');
 
       return { patches, keepOriginalStyles: true };
     }
@@ -136,47 +78,29 @@ contextBridge.exposeInMainWorld('myWindowAPI', {
         console.log('error in openDialog', err);
       });
 
-    console.log('checking folder');
-
     if (!folderPath) {
       return false;
     }
 
-    console.log('getting elements to patch');
-
     const elementsToPatch = getElementsToPatch(formData.variables);
 
-    // await patchSelectedDocument(formData.paths, elementsToPatch);
-
-    formData.paths.forEach((document) => {
-      // patchSelectedDocument(document.path, elementsToPatch);
-      patchDocument(fs.readFileSync(document.path), elementsToPatch)
-        .then((doc) => {
-          console.log('writing...');
-
+    for (const document of formData.paths) {
+      await patchDocument(fs.readFileSync(document.path), elementsToPatch).then(
+        (doc) => {
           fs.writeFileSync(
             `${folderPath}\\${path.parse(document.path).base}`,
             doc
-          );
+          ); // catch exception not needed
+        }
+      );
+    }
 
-          console.log('writed.');
-        })
-        .catch((error: Error) => {
-          console.log('error in patch', error);
-          notPatchError = error;
-        });
-    });
-
-    console.log(notPatchError);
-
-    console.log('writing json...');
     fs.writeFileSync(
       `${folderPath}\\${formData.name}_used.json`,
       JSON.stringify(formData)
-    ); // exception not needed
-    console.log('writed json.');
+    ); // catch exception not needed
 
-    return notPatchError;
+    return true;
   },
 
   getThemeSetting() {
