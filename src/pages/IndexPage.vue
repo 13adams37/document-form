7de0153a9a12ee212bbced23fe399e46e7261119
13adams37 <76618509+13adams37@users.noreply.github.com>
2023-12-06 13:05:45 +0300

    fix patcher, cleanup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,17 +4,6 @@
   <q-page class="full-width column flex flex-center">
     <h1>Главная страница</h1>
     <!-- здесь писать использование, используя компонент stepper -->
-    <!-- <div>
-      <div>Direct store</div>
-      <div>{{ store.counter }}</div>
-
-      <q-btn @click="store.counter = false">-</q-btn>
-      <q-btn @click="store.increment()">+</q-btn>
-    </div> -->
-    <div class="column full-width flex-center" v-for="item in 50">
-      blah blah
-    </div>
-    <div class="column full-width flex-center q-mb-xl">END LINE</div>
   </q-page>
 </template>
 
