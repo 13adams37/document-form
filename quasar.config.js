9453132df7eacade2981b90de30ee0df198a84b4
13adams37 <76618509+13adams37@users.noreply.github.com>
2023-12-27 16:12:08 +0300

    Added public path, quasar ssg build action

--- quasar.config.js
+++ quasar.config.js
@@ -59,7 +59,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/saturnProjectQuasar',
       // analyze: true,
       // env: {},
       // rawDefine: {}
